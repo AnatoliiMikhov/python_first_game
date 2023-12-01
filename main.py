93b3a275182dcdcd2353f6e47e898ea9351e6ccb
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-12-01 20:59:44 +0100

    random movement is implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import random
 import pygame
 from pygame.constants import QUIT
 
@@ -45,20 +46,20 @@ while playing:
         if event.type == QUIT:
             playing = False
 
-    if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
-        player.fill(COLOR_RED)
-
-    if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
-        player.fill(COLOR_ORANGE)
-
     if player_rect.top < 0:
-        player_speed[1] = -player_speed[1]
+        player_speed = random.choice(([-1, 1], [1, 1]))
         player.fill(COLOR_GREEN)
 
+    if player_rect.right >= WIDTH:
+        player_speed = random.choice(([-1, -1], [-1, 1]))
+        player.fill(COLOR_ORANGE)
+
+    if player_rect.bottom >= HEIGHT:
+        player_speed = random.choice(([1, -1], [-1, -1]))
+        player.fill(COLOR_RED)
+
     if player_rect.left < 0:
-        player_speed[0] = -player_speed[0]
+        player_speed = random.choice(([1, 1], [1, -1]))
         player.fill(COLOR_VIOLET)
 
     # fill display to main color
